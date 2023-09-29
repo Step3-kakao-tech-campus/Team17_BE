7e8c7cac852fc1cf7880553ebfabfa1ac4e3af9b
MinjuKwak01 <mkwak1125@gmail.com>
2023-09-30 01:46:42 +0900

    feat : 서명키 생성 메서드 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
@@ -1,11 +1,24 @@
 package com.kakaoseventeen.dogwalking._core.security;
 
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
 
 @Component
 public class JwtProvider {
+
     private static final String PREFIX = "Bearer ";
     private static final String SECRET = "thisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkey";
-    public static final Long accessTokenValidTime = 1000L * 60 * 30; //30분
-    public static final Long refreshTokenValidTime = 1000L * 60 * 60 * 24 *7; //7일
+    public static final Long accessTokenValidTime = 1000L * 60 * 30;
+    public static final Long refreshTokenValidTime = 1000L * 60 * 60 * 24 *7;
+
+
+    public static Key createKey(){
+        byte[] apiKeySecretBytes = Base64.getDecoder().decode(SECRET);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS512.getJcaName());
+        return signingKey;
+    }
 }
