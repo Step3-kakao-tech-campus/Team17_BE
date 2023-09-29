836874c5e3cd5ab0fad7ea6ada6a1f24a421bd0d
MinjuKwak01 <mkwak1125@gmail.com>
2023-09-30 00:11:27 +0900

    feat : JwtAuthenticationFilter 생성자 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtAuthenticationFilter.java
@@ -1,4 +1,13 @@
 package com.kakaoseventeen.dogwalking._core.security;
 
-public class JwtAuthenticationFilter {
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtProvider jwtProvider){
+        super(authenticationManager);
+        this.jwtProvider = jwtProvider;
+    }
 }
