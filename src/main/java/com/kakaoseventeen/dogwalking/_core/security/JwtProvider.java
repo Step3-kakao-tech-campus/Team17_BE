39524a087ec88cb6852cee482e0d1768454fadbe
MinjuKwak01 <mkwak1125@gmail.com>
2023-09-30 01:41:35 +0900

    chore : build.gradle에 jwt 라이브러리 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProvider {
+    private static final String PREFIX = "Bearer ";
+    private static final String SECRET = "thisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkeythisismysecretkey";
+    public static final Long accessTokenValidTime = 1000L * 60 * 30; //30분
+    public static final Long refreshTokenValidTime = 1000L * 60 * 60 * 24 *7; //7일
 }
