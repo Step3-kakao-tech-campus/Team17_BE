1e0d2404765b2744978255bfc04b58a3ad18a287
MinjuKwak01 <mkwak1125@gmail.com>
2023-09-30 20:56:32 +0900

    feat : access token 유효성 검사 메서드 생성

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtProvider.java
@@ -1,11 +1,15 @@
 package com.kakaoseventeen.dogwalking._core.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
 
 @Component
 public class JwtProvider {
@@ -21,4 +25,24 @@ public class JwtProvider {
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS512.getJcaName());
         return signingKey;
     }
+
+    // 헤더의 access 토큰의 유효성 + 만료일자 확인
+    public boolean isTokenValidate(String accessToken) {
+        try{
+            //JWT 토큰 파싱 및 검증을 시도
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(createKey())
+                    .build()
+                    .parseClaimsJws(accessToken);
+            // 토큰의 만료 시간을 현재 시간과 비교하여 유효성을 확인
+            // 만료 시간이 현재 시간 이후라면 토큰은 유효함
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            //예외가 발생하면 토큰이 유효하지 않다는 뜻
+            //TODO 예외처리 해줄 수 있음
+            return false;
+        }
+    }
+
+
 }
