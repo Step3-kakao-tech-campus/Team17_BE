b271ead0c7d529beaf4f2425c40f35794b525afa
MinjuKwak01 <mkwak1125@gmail.com>
2023-09-30 20:52:14 +0900

    feat : CustomUserDetails 클래스 생성

---
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package com.kakaoseventeen.dogwalking._core.security;
+
+import com.kakaoseventeen.dogwalking.member.domain.Member;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+//Spring Security에서 사용자의 정보를 담는 인터페이스
+@RequiredArgsConstructor
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    //이메일을 넘겨줌
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
