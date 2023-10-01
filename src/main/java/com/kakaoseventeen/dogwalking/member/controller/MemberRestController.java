964f269397d016bed7eddc57e0285656da53228f
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-01 15:05:33 +0900

    fix : member controller에 valid 어노테이션 추가

--- src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
@@ -4,6 +4,7 @@ import com.kakaoseventeen.dogwalking._core.utils.ApiUtils;
 import com.kakaoseventeen.dogwalking.member.dto.LoginRequestDTO;
 import com.kakaoseventeen.dogwalking.member.dto.LoginResponseDTO;
 import com.kakaoseventeen.dogwalking.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class MemberRestController {
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequestDTO){
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO){
         LoginResponseDTO responseDTO = memberService.login(loginRequestDTO);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
