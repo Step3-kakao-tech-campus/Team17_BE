af54183eefd65fc66f82277aa2c8093d6a4150e7
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-01 14:14:54 +0900

    fix : Member controller에 변수 추가

--- src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
@@ -3,6 +3,7 @@ package com.kakaoseventeen.dogwalking.member.controller;
 import com.kakaoseventeen.dogwalking._core.utils.ApiUtils;
 import com.kakaoseventeen.dogwalking.member.dto.LoginRequestDTO;
 import com.kakaoseventeen.dogwalking.member.dto.LoginResponseDTO;
+import com.kakaoseventeen.dogwalking.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MemberRestController {
+    private final MemberService memberService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequestDTO){
