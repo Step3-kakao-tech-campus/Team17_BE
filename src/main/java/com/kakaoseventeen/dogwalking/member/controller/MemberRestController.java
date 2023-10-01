b5a6ea7050fa0b95c834e63080a79268685123e7
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-01 13:53:36 +0900

    feat : member controller 추가

---
+++ src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
@@ -0,0 +1,22 @@
+package com.kakaoseventeen.dogwalking.member.controller;
+
+import com.kakaoseventeen.dogwalking._core.utils.ApiUtils;
+import com.kakaoseventeen.dogwalking.member.dto.LoginRequestDTO;
+import com.kakaoseventeen.dogwalking.member.dto.LoginResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberRestController {
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequestDTO){
+        LoginResponseDTO responseDTO = memberService.login(loginRequestDTO);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
+    }
+}
