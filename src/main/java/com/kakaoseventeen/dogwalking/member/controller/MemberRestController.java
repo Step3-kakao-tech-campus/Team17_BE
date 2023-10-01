3af599d71924f758474753d628f4ee37809fbbe8
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-01 20:35:42 +0900

    fix : api 양식에 맞게 변경

--- src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberRestController.java
@@ -9,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberRestController {
     private final MemberService memberService;
 
--- src/main/java/com/kakaoseventeen/dogwalking/token/controller/RefreshTokenRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/token/controller/RefreshTokenRestController.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class RefreshTokenRestController {
     private final RefreshTokenService refreshTokenService;
 
