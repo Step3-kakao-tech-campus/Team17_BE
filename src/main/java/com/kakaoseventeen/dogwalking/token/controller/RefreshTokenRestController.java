1c3ee3f871e8b682a9901b509426790e7d4fbb73
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-01 14:41:26 +0900

    feat : refresh token controller 생성

---
+++ src/main/java/com/kakaoseventeen/dogwalking/token/controller/RefreshTokenRestController.java
@@ -0,0 +1,20 @@
+package com.kakaoseventeen.dogwalking.token.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RefreshTokenRestController {
+
+    //access token 만료시간 지나면 프론트가 refresh token을 헤더에 담아 요청 보내줌
+    @GetMapping("/refresh")
+    public ResponseEntity<?> reissue(@RequestHeader("Authorization-refresh") String refreshToken){
+        String reissueToken = memberService.refresh(refreshToken);
+        //새로운 accesstoken을 반환
+        return ResponseEntity.ok().body(reissueToken);
+    }
+}
