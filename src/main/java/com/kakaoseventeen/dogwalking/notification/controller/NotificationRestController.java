63242cb0d18255f6060a019fab48cec9418b9830
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-08 16:05:04 +0900

    feat : 공고글 상세보기 controller 작성

--- src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationRestController.java
@@ -3,22 +3,29 @@ package com.kakaoseventeen.dogwalking.notification.controller;
 import com.kakaoseventeen.dogwalking._core.security.CustomUserDetails;
 import com.kakaoseventeen.dogwalking._core.utils.ApiUtils;
 import com.kakaoseventeen.dogwalking.notification.dto.LoadDogResponseDTO;
+import com.kakaoseventeen.dogwalking.notification.dto.LoadNotificationResponseDTO;
 import com.kakaoseventeen.dogwalking.notification.service.NotificationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class NotificationRestController {
     private final NotificationService notificationService;
 
-
     @GetMapping("/notification")
     public ResponseEntity<?> loadDog(@AuthenticationPrincipal CustomUserDetails userDetails){
         LoadDogResponseDTO responseDTO = notificationService.loadDog(userDetails.getMember());
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
+    }
+
+    @GetMapping("/notification/{id}")
+    public ResponseEntity<?> loadNotification(@PathVariable int id, @AuthenticationPrincipal CustomUserDetails userDetails){
+        LoadNotificationResponseDTO responseDTO = notificationService.loadNotification(id, userDetails.getMember());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
     }
