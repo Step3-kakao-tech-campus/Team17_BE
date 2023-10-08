6ad2d04f47746d0db84fdac942b8ec5457f3ad8f
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-08 15:12:11 +0900

    feat : loadDog 컨트롤러 작성

--- src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationRestController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationRestController.java
@@ -1,10 +1,26 @@
 package com.kakaoseventeen.dogwalking.notification.controller;
 
+import com.kakaoseventeen.dogwalking._core.security.CustomUserDetails;
+import com.kakaoseventeen.dogwalking._core.utils.ApiUtils;
+import com.kakaoseventeen.dogwalking.notification.dto.LoadDogResponseDTO;
+import com.kakaoseventeen.dogwalking.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class NotificationRestController {
+    private final NotificationService notificationService;
+
+
+    @GetMapping("/notification")
+    public ResponseEntity<?> loadDog(@AuthenticationPrincipal CustomUserDetails userDetails){
+        LoadDogResponseDTO responseDTO = notificationService.loadDog(userDetails.getMember());
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
+    }
 
 }
