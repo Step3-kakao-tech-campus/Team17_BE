a82174283a5f39f1a6b7608ab494650a8de2a94d
Cookie <pon05114@naver.com>
2023-10-15 23:28:26 +0900

    feat: ChatMessage Entity
    - JpaAuditing Annotation 추가
    - chatMessageId 필드에 GeneratedValue Annotation 추가
    - ContentType에서 MessageType 열거형 타입으로 변경
    - 생성자에 파라미터 수정

--- src/main/java/com/kakaoseventeen/dogwalking/chat/model/ChatMessage.java
+++ src/main/java/com/kakaoseventeen/dogwalking/chat/model/ChatMessage.java
@@ -7,21 +7,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "CHAT_MESSAGE")
+@Table(name = "CHAT_MESSAGE_tb")
 public class ChatMessage {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chatMessageId;
 
     private String content;
 
     @Enumerated(EnumType.STRING)
-    private ContentType contentType;
+    private MessageType messageType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private ChatRoom chatRoomId;
@@ -33,9 +36,9 @@ public class ChatMessage {
     private LocalDateTime createdAt;
 
     @Builder
-    public ChatMessage(String content, ContentType contentType, ChatRoom chatRoomId, Member senderId){
+    public ChatMessage(String content, MessageType messageType, ChatRoom chatRoomId, Member senderId){
         this.content = content;
-        this.contentType = contentType;
+        this.messageType = messageType;
         this.chatRoomId = chatRoomId;
         this.senderId = senderId;
     }
