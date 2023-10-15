213ce09b04f02cdcaf0744d8eacac54489dd29e7
Cookie <pon05114@naver.com>
2023-10-15 23:45:19 +0900

    feat: ChatRoom Entity
    - JpaAuditing을 위한 EntityListeners Annotation 추가
    - Table 어노테이션 속성 변경

--- src/main/java/com/kakaoseventeen/dogwalking/chat/model/ChatRoom.java
+++ src/main/java/com/kakaoseventeen/dogwalking/chat/model/ChatRoom.java
@@ -4,13 +4,15 @@ import com.kakaoseventeen.dogwalking.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "CHAT_ROOM")
+@Table(name = "CHAT_ROOM_tb")
+@EntityListeners(AuditingEntityListener.class)
 public class ChatRoom {
 
     @Id
