e52c101d957893d2689a5b15975866a5e0081110
DEVdongbaek <yardyard@likelion.org>
2023-10-19 01:55:47 +0900

    feat: 본인 프로필인지 분기 처리 하는  기능 구현

--- src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/member/controller/MemberController.java
@@ -1,16 +1,15 @@
 package com.kakaoseventeen.dogwalking.member.controller;
 
+import com.kakaoseventeen.dogwalking._core.security.CustomUserDetails;
 import com.kakaoseventeen.dogwalking._core.utils.ApiResponse;
 import com.kakaoseventeen.dogwalking._core.utils.ApiResponseGenerator;
-import com.kakaoseventeen.dogwalking.member.dto.LoginRequestDTO;
-import com.kakaoseventeen.dogwalking.member.dto.LoginResponseDTO;
-import com.kakaoseventeen.dogwalking.member.dto.UpdateProfileReqDTO;
-import com.kakaoseventeen.dogwalking.member.dto.UpdateProfileRespDTO;
+import com.kakaoseventeen.dogwalking.member.dto.*;
 import com.kakaoseventeen.dogwalking.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,6 +28,11 @@ public class MemberController {
     public ApiResponse<ApiResponse.CustomBody<UpdateProfileRespDTO>> updateProfile(@RequestBody UpdateProfileReqDTO reqDTO, @PathVariable("userId") Long userId) {
         UpdateProfileRespDTO respDTO = memberService.updateProfile(reqDTO, userId);
         return ApiResponseGenerator.success(respDTO, HttpStatus.OK);
+    }
 
+    @PostMapping("/profile/isOwner/{userId}")
+    public ApiResponse<ApiResponse.CustomBody<IsOwnerRespDTO>> isProfileOwner(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("userId") Long userId) {
+        IsOwnerRespDTO respDTO = memberService.isProfileOwner(customUserDetails, userId);
+        return ApiResponseGenerator.success(respDTO, HttpStatus.OK);
     }
 }
