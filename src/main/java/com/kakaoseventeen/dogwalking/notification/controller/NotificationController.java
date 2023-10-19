91d4cbbc9e254e4e9bee1f490bc5f605e508b952
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-19 23:19:28 +0900

    docs : service 메서드에 주석달기

--- src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/controller/NotificationController.java
@@ -20,12 +20,18 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
     private final NotificationService notificationService;
 
+    /**
+     * 공고글 강아지 불러오기 메서드
+     */
     @GetMapping("/notification")
     public ResponseEntity<?> loadDog(@AuthenticationPrincipal CustomUserDetails userDetails){
         LoadDogResponseDTO respDTO = notificationService.loadDog(userDetails.getMember());
         return ApiResponseGenerator.success(respDTO, HttpStatus.OK);
     }
 
+    /**
+     * 공고글 상세보기 메서드
+     */
     @GetMapping("/notification/{id}")
     public ResponseEntity<?> loadNotification(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails){
         LoadNotificationResponseDTO respDTO = notificationService.loadNotification(id, userDetails.getMember());
@@ -33,12 +39,19 @@ public class NotificationController {
 
     }
 
+    /**
+     * 공고글 작성하기 메서드
+     */
     @PostMapping("/notification")
     public ResponseEntity<?> writeNotification(@AuthenticationPrincipal CustomUserDetails userDetails, @Valid @RequestBody WriteNotificationDTO requestDTO){
         notificationService.writeNotification(requestDTO, userDetails.getMember());
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+
+    /**
+     * 공고글 수정하기 메서드
+     */
     @PutMapping("/notification/{id}")
     public ResponseEntity<?> editNotification(@PathVariable Long id, @Valid @RequestBody UpdateNotificationDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
         notificationService.editNotification(id, requestDTO, userDetails.getMember());
