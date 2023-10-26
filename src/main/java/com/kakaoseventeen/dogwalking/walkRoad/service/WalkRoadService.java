97a3bf7ed228a7c78120913f6600eac735b96447
DEVdongbaek <yardyard@likelion.org>
2023-10-26 20:31:29 +0900

    fix: 산책 현황 조회 및 생성 API간 URL Path 변수 변경

--- src/main/java/com/kakaoseventeen/dogwalking/walkRoad/service/WalkRoadService.java
+++ src/main/java/com/kakaoseventeen/dogwalking/walkRoad/service/WalkRoadService.java
@@ -3,6 +3,7 @@ package com.kakaoseventeen.dogwalking.walkRoad.service;
 
 import com.kakaoseventeen.dogwalking._core.utils.MessageCode;
 import com.kakaoseventeen.dogwalking._core.utils.exception.WalkNotExistException;
+import com.kakaoseventeen.dogwalking.match.repository.MatchingRepository;
 import com.kakaoseventeen.dogwalking.walk.domain.Walk;
 import com.kakaoseventeen.dogwalking.walk.repository.WalkRepository;
 import com.kakaoseventeen.dogwalking.walkRoad.domain.WalkRoad;
@@ -30,20 +31,22 @@ public class WalkRoadService {
 
     private final WalkRepository walkRepository;
 
+    private final MatchingRepository matchingRepository;
+
     /**
      * 산책 경로 저장하기 메서드
      */
     @Transactional
-    public WalkRoadRespDTO.SaveWalkResp saveWalkRoad(WalkRoadReqDTO walkRoadReqDTO, Long walkId) throws WalkNotExistException {
-        Walk walk = walkRepository.findById(walkId).orElseThrow(() -> new WalkNotExistException(MessageCode.WALK_NOT_EXIST));
+    public WalkRoadRespDTO.SaveWalkResp saveWalkRoad(WalkRoadReqDTO walkRoadReqDTO, Long matchId) throws WalkNotExistException {
+        Walk walk = matchingRepository.findWalkFromMatchById(matchId).orElseThrow(() -> new WalkNotExistException(MessageCode.WALK_NOT_EXIST));
 
         WalkRoad walkRoad = walkRoadRepository.save(WalkRoad.of(walkRoadReqDTO.getLat(), walkRoadReqDTO.getLng(), walk));
         return new WalkRoadRespDTO.SaveWalkResp(walkRoad);
     }
 
     @Transactional(readOnly = true)
-    public WalkRoadRespDTO.FindByWalkId findAllByWalkId(Long walkId) throws WalkNotExistException {
-        Walk walk = walkRepository.findById(walkId).orElseThrow(() -> new WalkNotExistException(MessageCode.WALK_NOT_EXIST));
+    public WalkRoadRespDTO.FindByWalkId findAllByWalkId(Long matchId) throws WalkNotExistException {
+        Walk walk = matchingRepository.findWalkFromMatchById(matchId).orElseThrow(() -> new WalkNotExistException(MessageCode.WALK_NOT_EXIST));
 
         List<WalkRoad> walks = walkRoadRepository.findWalkRoadsByWalk(walk.getId());
         return new WalkRoadRespDTO.FindByWalkId(walks);
