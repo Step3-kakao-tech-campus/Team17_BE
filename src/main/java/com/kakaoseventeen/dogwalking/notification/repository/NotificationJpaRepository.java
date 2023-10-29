9c8bcffbc8c4ec9d2977bf700e0299e71465915a
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-29 15:18:36 +0900

    fix : 공고글 필터 및 검색 native query로 수정

--- src/main/java/com/kakaoseventeen/dogwalking/notification/repository/NotificationJpaRepository.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/repository/NotificationJpaRepository.java
@@ -16,65 +16,72 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
             "where n.walk.master.id = :userId or n.walk.walker.id =:userId")
     List<Notification> findNotificationByMemberId(Long userId);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "ORDER BY (6371.0 *acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasNone(@Param("latitude") Double lat, @Param("longitude") Double lng, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
             "WHERE d.breed IN :breed " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasBreed(@Param("latitude") Double lat, @Param("longitude") Double lng, @Param("breed") List<String> breed, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
             "WHERE d.size IN :size " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSize(@Param("latitude") Double lat, @Param("longitude") Double lng, @Param("size") List<String> size, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (d.size IN :size OR d.breed IN :breed) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE d.size IN :size OR d.breed IN :breed " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreed(@Param("latitude") Double lat, @Param("longitude") Double lng, @Param("size") List<String> size, @Param("breed") List<String> breed, Pageable pageable);
 
 
     /************************************************************************************/
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (6371.0 *acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "ORDER BY (6371.0 *acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasNoneKey(@Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
             "WHERE d.breed IN :breed " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasBreedKey(@Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
             "WHERE d.size IN :size " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeKey(@Param("size") List<String> size, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
             "WHERE (d.size IN :size OR d.breed IN :breed) " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreedKey(@Param("size") List<String> size, @Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
     // @Query(value = "SELECT N.ID, N.lat, N.lng, D.name, D.sex, D.age,  FROM Notification AS N LEFT JOIN Dog as D ON N.notification_id = D.id",nativeQuery = true)
@@ -85,75 +92,81 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
             "FROM Notification AS n", nativeQuery = true)
     List<Notification> findAllHasNoneKey(@Param("latitude")Double lat, @Param("longitude")Double lng, Long key, Pageable pageable);
      */
+	/**********************************************************************************************************/
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE n.title LIKE CONCAT('%', :tit, '%') " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasNoneSearch(@Param("tit") String tit, @Param("latitude") Double lat, @Param("longitude") Double lng, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE d.breed IN :breed " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE n.title LIKE CONCAT('%', :tit, '%') " +
+            "AND d.breed IN :breed " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasBreedSearch(@Param("tit") String tit, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("breed") List<String> breed, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE d.size IN :size " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE n.title LIKE CONCAT('%', :tit, '%') " +
+            "AND d.size IN :size " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeSearch(@Param("tit") String tit, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("size") List<String> size, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (d.size IN :size OR d.breed IN :breed) " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE n.title LIKE CONCAT('%', :tit, '%') " +
+            "AND (d.size IN :size OR d.breed IN :breed) " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreedSearch(@Param("tit") String tit, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("size") List<String> size, @Param("breed") List<String> breed, Pageable pageable);
 
 
     /********************************************************************************/
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE n.title LIKE CONCAT('%', :tit, '%') " +
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasNoneKeySearch(@Param("tit") String tit, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE d.breed IN :breed " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE (n.title LIKE CONCAT('%', :tit, '%')) AND (n.breed IN :breed) " +
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasBreedKeySearch(@Param("tit") String tit, @Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE d.size IN :size " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE (n.title LIKE CONCAT('%', :tit, '%')) AND (n.size IN :size) " +
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeKeySearch(@Param("tit") String tit, @Param("size") List<String> size, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "JOIN FETCH n.dog d " +
-            "WHERE (d.size IN :size OR d.breed IN :breed) " +
-            "AND (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) > :key " +
-            "AND (n.title LIKE %:tit%) " +
-            "ORDER BY (6371.0*acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat)))) ASC")
+    @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(34.24))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
+            "FROM Notification_tb n " +
+            "JOIN dog_tb d on n.dog_id = d.id " +
+            "WHERE (n.title LIKE CONCAT('%', :tit, '%')) AND (n.size IN :size) + OR (n.breed IN :breed) " +
+            "GROUP BY n.chatroom_id " +
+            "HAVING distance > :key " +
+            "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreedKeySearch(@Param("tit") String tit, @Param("size") List<String> size, @Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
-
 
 }
