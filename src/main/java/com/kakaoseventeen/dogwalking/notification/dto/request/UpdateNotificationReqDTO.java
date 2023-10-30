cffc116f39f91ab7152b9bf2f155fa637ef91507
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-30 20:32:24 +0900

    fix : 공고글 수정 dto 수정

--- src/main/java/com/kakaoseventeen/dogwalking/notification/dto/request/UpdateNotificationReqDTO.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/dto/request/UpdateNotificationReqDTO.java
@@ -1,5 +1,6 @@
 package com.kakaoseventeen.dogwalking.notification.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -23,7 +24,8 @@ public class UpdateNotificationReqDTO {
     private LocalDateTime start;
     @NotNull(message = "종료 시간을 입력해주세요")
     private LocalDateTime end;
-    private String significant;
-    @NotNull(message = "코인을 입력해주세요")
+    @NotNull(message="코인을 입력해 주세요.")
+    @Min(value=1, message="코인은 최소한 1개 이상이어야 합니다.")
     private BigDecimal coin;
+    private String significant;
 }
