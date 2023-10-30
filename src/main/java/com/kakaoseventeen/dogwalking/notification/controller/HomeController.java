0c8bac50ad35aabf8258466f625cbd8060bbc935
MinjuKwak01 <mkwak1125@gmail.com>
2023-10-30 20:30:26 +0900

    fix : HomeController request param에 value값 추가

--- src/main/java/com/kakaoseventeen/dogwalking/notification/controller/HomeController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.kakaoseventeen.dogwalking.notification.controller;
 import com.kakaoseventeen.dogwalking._core.security.CustomUserDetails;
 import com.kakaoseventeen.dogwalking._core.utils.ApiResponseGenerator;
 import com.kakaoseventeen.dogwalking._core.utils.CursorRequest;
+import com.kakaoseventeen.dogwalking._core.utils.MessageCode;
+import com.kakaoseventeen.dogwalking._core.utils.exception.ParameterException;
 import com.kakaoseventeen.dogwalking.notification.dto.response.HomeRespDTO;
 import com.kakaoseventeen.dogwalking.notification.service.HomeService;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +25,13 @@ public class HomeController {
     @GetMapping("/home")
     public ResponseEntity<?> getPosts(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                       CursorRequest cursorRequest,
-                                      @RequestParam Double latitude,
-                                      @RequestParam Double longitude,
-                                      @RequestParam(required = false) List<String> big,
-                                      @RequestParam(required = false) List<String> breed,
-                                      @RequestParam(required = false) String search
+                                      @RequestParam(value = "latitude") Double latitude,
+                                      @RequestParam(value = "longitude") Double longitude,
+                                      @RequestParam(required = false, value = "big") List<String> big,
+                                      @RequestParam(required = false, value = "breed") List<String> breed,
+                                      @RequestParam(required = false, value = "word") String search
                                       ) {
-
+        
         if(big == null) big = List.of();
         if(breed == null) breed = List.of();
         if(search == null) search = "";
