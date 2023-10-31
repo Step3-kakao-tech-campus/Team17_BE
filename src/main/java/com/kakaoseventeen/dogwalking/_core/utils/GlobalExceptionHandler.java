46abee377f81956f70f6a50a2a52233f6dda2cdd
Cookie <pon05114@naver.com>
2023-10-31 19:56:56 +0900

    feat: GlobalExceptionHandler add Exception
    - 회원가입 관련 예외 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/utils/GlobalExceptionHandler.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.kakaoseventeen.dogwalking._core.utils;
 
-import com.kakaoseventeen.dogwalking._core.utils.exception.MatchNotExistException;
-import com.kakaoseventeen.dogwalking._core.utils.exception.WalkNotExistException;
+import com.kakaoseventeen.dogwalking._core.utils.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,6 +17,24 @@ public class GlobalExceptionHandler {
     public ApiResponse<ApiResponse.CustomBody> handleIllegalStateException(MatchNotExistException e){
         return ApiResponseGenerator.fail(MessageCode.MATCH_NOT_EXIST.getValue(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(InvalidEmailFormatException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleIllegalStateException(InvalidEmailFormatException e){
+        return ApiResponseGenerator.fail(MemberMessageCode.INVALID_EMAIL_FORMAT.getValue(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(InvalidPasswordFormatException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleIllegalStateException(InvalidPasswordFormatException e){
+        return ApiResponseGenerator.fail(MemberMessageCode.INVALID_PASSWORD_FORMAT.getValue(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(InvalidPasswordLengthException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleIllegalStateException(InvalidPasswordLengthException e){
+        return ApiResponseGenerator.fail(MemberMessageCode.INVALID_PASSWORD_LENGTH.getValue(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleIllegalStateException(DuplicateEmailException e){
+        return ApiResponseGenerator.fail(MemberMessageCode.DUPLICATE_EMAIL.getValue(), HttpStatus.BAD_REQUEST);
+    }
+
 
 
 }
