eeb348c77084925a9ad10d844031ad2b77fbd476
MinjuKwak01 <mkwak1125@gmail.com>
2023-11-01 05:35:03 +0900

    feat : 예외처리 enum 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/utils/MemberMessageCode.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/MemberMessageCode.java
@@ -7,6 +7,8 @@ public enum MemberMessageCode {
     INVALID_EMAIL_FORMAT("아이디는 이메일 형식으로 작성해야 합니다."),
     INVALID_PASSWORD_LENGTH("비밀번호는 8~16자로 작성해야 합니다."),
     INVALID_PASSWORD_FORMAT("비밀번호는 특수문자, 영문자, 숫자가 포함되어야 합니다."),
+    MEMBER_NOT_EXIST("회원가입이 안된 유저입니다."),
+    PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다."),
     DUPLICATE_EMAIL("중복된 이메일 입니다.");
 
 
