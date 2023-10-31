e77e8b2517691416d4a73afe079db3ff5f8fb785
MinjuKwak01 <mkwak1125@gmail.com>
2023-11-01 05:40:05 +0900

    docs : 의미없는 주석 수정2

--- src/main/java/com/kakaoseventeen/dogwalking/notification/repository/NotificationJpaRepository.java
+++ src/main/java/com/kakaoseventeen/dogwalking/notification/repository/NotificationJpaRepository.java
@@ -16,6 +16,8 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
             "where n.walk.master.id = :userId or n.walk.walker.id =:userId")
     List<Notification> findNotificationByMemberId(Long userId);
 
+
+
     @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
             "FROM Notification_tb n " +
             "JOIN dog_tb d on n.dog_id = d.id " +
@@ -84,14 +86,7 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
             "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreedKey(@Param("size") List<String> size, @Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
 
-    // @Query(value = "SELECT N.ID, N.lat, N.lng, D.name, D.sex, D.age,  FROM Notification AS N LEFT JOIN Dog as D ON N.notification_id = D.id",nativeQuery = true)
 
-    /*
-    @Query(value = "SELECT n.id, n.lat, n.lng, d.image, d.age, d.sex " +
-            "ST_Distance_Sphere(POINT(:longitude, :latitude),POINT(n.lat, n.lng)) AS dis " +
-            "FROM Notification AS n", nativeQuery = true)
-    List<Notification> findAllHasNoneKey(@Param("latitude")Double lat, @Param("longitude")Double lng, Long key, Pageable pageable);
-     */
 	/**********************************************************************************************************/
 
     @Query(value = "SELECT n.*, 6371.0 *acos(cos(radians(:latitude))*cos(radians(n.lat))*cos(radians(n.lng)-radians(:longitude))+sin(radians(:latitude))*sin(radians(n.lat))) as distance " +
@@ -168,5 +163,6 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
             "HAVING distance > :key " +
             "ORDER BY distance ASC", nativeQuery = true)
     List<Notification> findAllHasSizeAndBreedKeySearch(@Param("tit") String tit, @Param("size") List<String> size, @Param("breed") List<String> breed, @Param("latitude") Double lat, @Param("longitude") Double lng, @Param("key") Double key, Pageable pageable);
+
 
 }
