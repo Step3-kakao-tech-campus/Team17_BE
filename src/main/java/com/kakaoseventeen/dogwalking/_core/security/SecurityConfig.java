cad934e5e706994273cd3a5efaa3026d74ce266a
Cookie <pon05114@naver.com>
2023-10-31 19:58:28 +0900

    feat: Password Encoder
    - 회원가입시 비밀번호 암호화
    - BCryptPasswordEncoder 적용

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/SecurityConfig.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -85,6 +87,11 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 
