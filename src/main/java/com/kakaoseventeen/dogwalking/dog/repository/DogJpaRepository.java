7109f5b7cdd35a226322447c9360984e68ac9a9a
DEVdongbaek <yardyard@likelion.org>
2023-11-02 15:57:00 +0900

    fix: 유저 프로필 조회시 DTO 코드 변경

--- src/main/java/com/kakaoseventeen/dogwalking/dog/repository/DogJpaRepository.java
+++ src/main/java/com/kakaoseventeen/dogwalking/dog/repository/DogJpaRepository.java
@@ -1,11 +1,12 @@
 package com.kakaoseventeen.dogwalking.dog.repository;
 
 import com.kakaoseventeen.dogwalking.dog.domain.Dog;
+import com.kakaoseventeen.dogwalking.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface DogJpaRepository extends JpaRepository<Dog, Long> {
 
-    List<Dog> findDogsByMemberId(long memberId);
+    List<Dog> findDogsByMemberId(Member member);
 }
