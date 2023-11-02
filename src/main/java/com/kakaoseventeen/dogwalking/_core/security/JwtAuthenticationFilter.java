ee920c8de6422f92668093748b01ab7f1f50cdaa
MinjuKwak01 <mkwak1125@gmail.com>
2023-11-02 18:27:43 +0900

    fix : JwtException 처리

--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/JwtAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package com.kakaoseventeen.dogwalking._core.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakaoseventeen.dogwalking._core.utils.ApiResponseGenerator;
 import com.kakaoseventeen.dogwalking._core.utils.MessageCode;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,15 +35,24 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
             chain.doFilter(request, response);
             return;
         }
-        String extractedToken = token.replace("Bearer ", "");
-        // 유효한 토큰인지 확인
-        if (jwtProvider.isTokenValidate(extractedToken, request)) {
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아온다 -> Authentication 객체에 저장
-            Authentication authentication = jwtProvider.getAuthentication(extractedToken);
-            // SecurityContext 에 Authentication 객체를 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-        chain.doFilter(request, response);
+        try{
+            String extractedToken = token.replace("Bearer ", "");
+            // 유효한 토큰인지 확인
+            if (jwtProvider.isTokenValidate(extractedToken, request)) {
+                // 토큰이 유효하면 토큰으로부터 유저 정보를 받아온다 -> Authentication 객체에 저장
+                Authentication authentication = jwtProvider.getAuthentication(extractedToken);
+                // SecurityContext 에 Authentication 객체를 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            chain.doFilter(request, response);
+        }catch (final JwtException e) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+            response.getWriter().println("{ \"success\": false, \"response\": null, \"error\": { \"message\": \"" +
+                    e.getMessage() + "\", \"status\": \"" + HttpStatus.UNAUTHORIZED  + "\" } }");
+    }
+
     }
 
 
--- src/main/java/com/kakaoseventeen/dogwalking/_core/security/handler/FilterResponse.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/security/handler/FilterResponse.java
@@ -1,6 +1,6 @@
 package com.kakaoseventeen.dogwalking._core.security.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.kakaoseventeen.dogwalking._core.utils.exception.SecurityFilterException;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -11,8 +11,6 @@ public class FilterResponse {
     public static void unAuthorized(HttpServletResponse resp, SecurityFilterException e) throws IOException {
         resp.setStatus(HttpStatus.UNAUTHORIZED.value());
         resp.setContentType("application/json; charset=utf-8");
-     //   ObjectMapper om = new ObjectMapper();
-     //   String responseBody = om.writeValueAsString(e.message);
         resp.getWriter().println("{ \"success\": false, \"response\": null, \"error\": { \"message\": \"" +
                 e.message + "\", \"status\": \"" + HttpStatus.UNAUTHORIZED  + "\" } }");
     }
@@ -20,8 +18,6 @@ public class FilterResponse {
     public static void forbidden(HttpServletResponse resp, SecurityFilterException e) throws IOException {
         resp.setStatus(HttpStatus.FORBIDDEN.value());
         resp.setContentType("application/json; charset=utf-8");
-      //  ObjectMapper om = new ObjectMapper();
-       // String responseBody = om.writeValueAsString(e.message);
         resp.getWriter().println("{ \"success\": false, \"response\": null, \"error\": { \"message\": \"" +
                 e.message + "\", \"status\": \"" + HttpStatus.FORBIDDEN  + "\" } }");
     }
