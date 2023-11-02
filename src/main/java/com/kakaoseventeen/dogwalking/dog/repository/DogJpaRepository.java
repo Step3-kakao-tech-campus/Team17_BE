147f06f4f298ec58fdc0d225605d2718799c9d09
DEVdongbaek <yardyard@likelion.org>
2023-11-02 17:16:06 +0900

    fix: notification으로 User 조회 쿼리 네이밍 쿼리에서 JPQL 쿼리로 수정

--- src/main/java/com/kakaoseventeen/dogwalking/dog/repository/DogJpaRepository.java
+++ src/main/java/com/kakaoseventeen/dogwalking/dog/repository/DogJpaRepository.java
@@ -3,10 +3,12 @@ package com.kakaoseventeen.dogwalking.dog.repository;
 import com.kakaoseventeen.dogwalking.dog.domain.Dog;
 import com.kakaoseventeen.dogwalking.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DogJpaRepository extends JpaRepository<Dog, Long> {
 
-    List<Dog> findDogsByMemberId(Member member);
+    @Query("select d from Dog d where d.member =:member")
+    List<Dog> findDogsByMember(Member member);
 }
