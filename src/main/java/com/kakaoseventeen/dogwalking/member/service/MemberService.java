7385a16711184a62fba875159dd4bc3833e3e725
DEVdongbaek <yardyard@likelion.org>
2023-11-02 17:01:34 +0900

    fix: notification으로 User 조회 쿼리 수정

--- src/main/java/com/kakaoseventeen/dogwalking/member/service/MemberService.java
+++ src/main/java/com/kakaoseventeen/dogwalking/member/service/MemberService.java
@@ -168,7 +168,6 @@ public class MemberService {
 
         List<Application> applications = applicationRepository.findApplicationByMemberId(member.getId());
 
-
         List<Review> reviews = reviewRepository.findReviewByMemberId(member.getId());
 
         return MemberProfileRespDTO.of(member, notifications, dogs, applications, reviews);
