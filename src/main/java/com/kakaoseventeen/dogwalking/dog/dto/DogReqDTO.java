c1834abadb145c1e5097327b5354176bae985018
DEVdongbaek <yardyard@likelion.org>
2023-11-05 02:20:12 +0900

    fix: 415 Unsupported Media Type Error ModelAttribute DTO 객체 Builder 선언

--- src/main/java/com/kakaoseventeen/dogwalking/dog/dto/DogReqDTO.java
+++ src/main/java/com/kakaoseventeen/dogwalking/dog/dto/DogReqDTO.java
@@ -1,11 +1,14 @@
 package com.kakaoseventeen.dogwalking.dog.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Getter @Setter
 public class DogReqDTO implements Serializable {
 
