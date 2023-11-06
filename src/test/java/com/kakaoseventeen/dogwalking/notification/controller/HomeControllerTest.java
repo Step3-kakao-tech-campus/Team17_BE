0b0d0a3e5202155db345b87bd33caec81c754efa
MinjuKwak <mkwak1125@gmail.com>
2023-11-06 20:50:12 +0900

    test : 홈 화면 통합테스트3 (견종, 사이즈 필터, 검색)

--- src/test/java/com/kakaoseventeen/dogwalking/notification/controller/HomeControllerTest.java
+++ src/test/java/com/kakaoseventeen/dogwalking/notification/controller/HomeControllerTest.java
@@ -89,4 +89,42 @@ class HomeControllerTest {
     }
 
 
+
+    @DisplayName("홈 메인화면 테스트 - 견종, 강아지 크기, 제목 키워드")
+    @Test
+    void get_post_test3() throws Exception {
+        // given
+        Double lat = 0.0;
+        Double lng = 0.0;
+        List<String> big = new ArrayList<>();
+        big.add("소형견");
+        big.add("중형견");
+        String breed = "푸들";
+        String keyword = "공통";
+
+
+        // when
+        mvc.perform(
+                get("/init")
+        );
+
+        ResultActions resultActions = mvc.perform(
+                get(String.format("/api/home"))
+                        .param("latitude",lat.toString())
+                        .param("longitude",lng.toString())
+                        .param("big",big.get(0))
+                        .param("big",big.get(1))
+                        .param("breed",breed)
+                        .param("word", keyword)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // then
+        String responseBody = new String(resultActions.andReturn().getResponse().getContentAsByteArray(), StandardCharsets.UTF_8);
+        System.out.println("테스트 : " + responseBody);
+
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+    }
+
+
 }
