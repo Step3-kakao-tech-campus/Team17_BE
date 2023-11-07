b6c9dcafa4f80a306f2bea166ede1be592403ee7
Cookie <pon05114@naver.com>
2023-11-07 19:54:51 +0900

    feat: ChatRoomMessageCode Enum
    - 채팅방 오류 메세지 Enum 구현

---
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/exception/ChatRoomMessageCode.java
@@ -0,0 +1,14 @@
+package com.kakaoseventeen.dogwalking._core.utils.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ChatRoomMessageCode {
+
+    MEMBER_NOT_FOUND("사용자가 존재하지 않아 채팅방 생성에 실패하였습니다.");
+    private final String value;
+
+    ChatRoomMessageCode(String value) {
+        this.value = value;
+    }
+}
