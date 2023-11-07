fb4a2c5e8ca96197aca5e95aa9a95db57f7f030a
Cookie <pon05114@naver.com>
2023-11-07 19:36:36 +0900

    fix: ChatRoomWriteService
    - 채팅방 생성 Transactional 어노테이션 추가

--- src/main/java/com/kakaoseventeen/dogwalking/chat/service/ChatRoomWriteService.java
+++ src/main/java/com/kakaoseventeen/dogwalking/chat/service/ChatRoomWriteService.java
@@ -7,6 +7,7 @@ import com.kakaoseventeen.dogwalking.member.domain.Member;
 import com.kakaoseventeen.dogwalking.member.repository.MemberJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +16,12 @@ public class ChatRoomWriteService {
     private final ChatRoomRepository chatRoomRepository;
     private final MemberJpaRepository memberJpaRepository;
 
+    @Transactional
     public void save(ChatRoomReqDTO chatRoomReqDTO){
 
         // TODO - 커스텀 Exception 적용할 것
-        Member appMember = memberJpaRepository.findById(chatRoomReqDTO.appMemberId()).orElseThrow();
-        Member notiMember = memberJpaRepository.findById(chatRoomReqDTO.notiMemberId()).orElseThrow();
+        Member appMember = memberJpaRepository.findById(chatRoomReqDTO.appMemberId()).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+        Member notiMember = memberJpaRepository.findById(chatRoomReqDTO.notiMemberId()).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
         ChatRoom chatRoom = getChatRoom(appMember, notiMember);
 
