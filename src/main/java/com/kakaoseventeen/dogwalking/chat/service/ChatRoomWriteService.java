f442af3c355167f8c6336c5c37fc319acb496779
Cookie <pon05114@naver.com>
2023-11-07 22:49:10 +0900

    fix: ChatRoomWriteService save method fix
    - 매칭 아이디 조회 추가
    - Dto to ChatRoom 코드 수정

--- src/main/java/com/kakaoseventeen/dogwalking/chat/service/ChatRoomWriteService.java
+++ src/main/java/com/kakaoseventeen/dogwalking/chat/service/ChatRoomWriteService.java
@@ -1,10 +1,13 @@
 package com.kakaoseventeen.dogwalking.chat.service;
 
 import com.kakaoseventeen.dogwalking._core.utils.exception.ChatRoomMessageCode;
+import com.kakaoseventeen.dogwalking._core.utils.exception.chatroom.ChatRoomMatchNotFoundException;
 import com.kakaoseventeen.dogwalking._core.utils.exception.chatroom.ChatRoomMemberNotFoundException;
 import com.kakaoseventeen.dogwalking.chat.domain.ChatRoom;
 import com.kakaoseventeen.dogwalking.chat.dto.ChatRoomReqDTO;
 import com.kakaoseventeen.dogwalking.chat.repository.ChatRoomRepository;
+import com.kakaoseventeen.dogwalking.match.domain.Match;
+import com.kakaoseventeen.dogwalking.match.repository.MatchingRepository;
 import com.kakaoseventeen.dogwalking.member.domain.Member;
 import com.kakaoseventeen.dogwalking.member.repository.MemberJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,22 +20,28 @@ public class ChatRoomWriteService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final MemberJpaRepository memberJpaRepository;
+    private final MatchingRepository matchingRepository;
 
     @Transactional
     public void save(ChatRoomReqDTO chatRoomReqDTO){
 
-        Member appMember = memberJpaRepository.findById(chatRoomReqDTO.appMemberId()).orElseThrow(() -> new ChatRoomMemberNotFoundException(ChatRoomMessageCode.MEMBER_NOT_FOUND));
-        Member notiMember = memberJpaRepository.findById(chatRoomReqDTO.notiMemberId()).orElseThrow(() -> new ChatRoomMemberNotFoundException(ChatRoomMessageCode.MEMBER_NOT_FOUND));
+        Member appMember = memberJpaRepository.findById(chatRoomReqDTO.appMemberId())
+                .orElseThrow(() -> new ChatRoomMemberNotFoundException(ChatRoomMessageCode.MEMBER_NOT_FOUND));
+        Member notiMember = memberJpaRepository.findById(chatRoomReqDTO.notiMemberId())
+                .orElseThrow(() -> new ChatRoomMemberNotFoundException(ChatRoomMessageCode.MEMBER_NOT_FOUND));
+        Match match = matchingRepository.findById(chatRoomReqDTO.matchId())
+                .orElseThrow(() ->new ChatRoomMatchNotFoundException(ChatRoomMessageCode.MATCH_NOT_FOUND));
 
-        ChatRoom chatRoom = getChatRoom(appMember, notiMember);
+        ChatRoom chatRoom = getChatRoom(appMember, notiMember, match);
 
         chatRoomRepository.save(chatRoom);
     }
 
-    private static ChatRoom getChatRoom(Member appMember, Member notiMember) {
+    private static ChatRoom getChatRoom(Member appMember, Member notiMember, Match match) {
         ChatRoom chatRoom = ChatRoom.builder()
                 .appMemberId(appMember)
                 .notiMemberId(notiMember)
+                .matchId(match)
                 .build();
         return chatRoom;
     }
