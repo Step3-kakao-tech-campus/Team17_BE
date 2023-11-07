e945a8255118f3453c88a1986e7c6b331616a77a
Cookie <pon05114@naver.com>
2023-11-08 01:20:51 +0900

    feat: ReviewController getReview method
    - 리뷰 작성 전 정보 조회 Controller

--- src/main/java/com/kakaoseventeen/dogwalking/review/controller/ReviewController.java
+++ src/main/java/com/kakaoseventeen/dogwalking/review/controller/ReviewController.java
@@ -1,22 +1,23 @@
 package com.kakaoseventeen.dogwalking.review.controller;
 
+import com.kakaoseventeen.dogwalking._core.utils.ApiResponse;
 import com.kakaoseventeen.dogwalking._core.utils.ApiResponseGenerator;
+import com.kakaoseventeen.dogwalking.review.dto.GetReviewResDTO;
 import com.kakaoseventeen.dogwalking.review.dto.WriteReviewReqDTO;
+import com.kakaoseventeen.dogwalking.review.service.ReviewReadService;
 import com.kakaoseventeen.dogwalking.review.service.ReviewWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewWriteService reviewWriteService;
+    private final ReviewReadService reviewReadService;
 
     @PostMapping("/review")
     public ResponseEntity<?> writeReview(@RequestBody WriteReviewReqDTO writeReviewReqDTO){
@@ -24,6 +25,15 @@ public class ReviewController {
         reviewWriteService.writeReview(writeReviewReqDTO);
 
         return ApiResponseGenerator.success(HttpStatus.OK);
+
+    }
+
+    @GetMapping("/review/{memberId}")
+    public ApiResponse<ApiResponse.CustomBody<GetReviewResDTO>> getReview(@PathVariable Long memberId){
+
+        GetReviewResDTO response = reviewReadService.getReviewPrivew(memberId);
+
+        return ApiResponseGenerator.success(response, HttpStatus.OK);
 
     }
 }
