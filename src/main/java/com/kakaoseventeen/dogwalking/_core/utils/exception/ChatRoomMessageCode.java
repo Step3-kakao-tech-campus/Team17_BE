416101b6a6d0d29fa58208eaf48b3839da944e40
Cookie <pon05114@naver.com>
2023-11-07 22:45:36 +0900

    feat: ChatRoomMessageCode
    - 채팅방 목록 조회시 매칭아이디 조회 실패 예외 메세지 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/utils/exception/ChatRoomMessageCode.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/exception/ChatRoomMessageCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ChatRoomMessageCode {
 
-    MEMBER_NOT_FOUND("사용자가 존재하지 않아 채팅방 생성에 실패하였습니다.");
+    MEMBER_NOT_FOUND("사용자가 존재하지 않아 채팅방 생성에 실패하였습니다."),
+    MATCH_NOT_FOUND("일치하는 매칭 결과가 존재하지 않아 채팅방 생성에 실패하였습니다.");
     private final String value;
 
     ChatRoomMessageCode(String value) {
