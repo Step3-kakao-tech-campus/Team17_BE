6dd989e4e2b44f66048d8573ba399fbf88f4bfa2
Cookie <pon05114@naver.com>
2023-11-08 01:21:42 +0900

    feat: ReviewMessageCode Review member not found message
    - 리뷰 작성 전 정보 조회 예외메세지 추가

--- src/main/java/com/kakaoseventeen/dogwalking/_core/utils/ReviewMessageCode.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/ReviewMessageCode.java
@@ -8,7 +8,8 @@ public enum ReviewMessageCode {
     MEMBER_ID_NOT_EXIST("잘못된 사용자 접근입니다."),
     RECEIVE_MEMBER_ID_NOT_EXIST("리뷰받는 사용자가 존재하지 않습니다."),
     NOTIFICATION_ID_NOT_EXIST("잘못된 요청 입니다."),
-    REVIEW_CONTENT_NOT_EXIST("리뷰 내용을 작성해주세요.");
+    REVIEW_CONTENT_NOT_EXIST("리뷰 내용을 작성해주세요."),
+    REVIEW_MEMBER_NOT_FOUND("리뷰받을 사용자를 찾을 수 없습니다.");
 
     private String value;
 
