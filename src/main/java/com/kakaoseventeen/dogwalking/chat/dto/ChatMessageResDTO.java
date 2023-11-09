fc5ad94dda0ef75dc1165cd9b597c91221e09861
Cookie <pon05114@naver.com>
2023-11-10 06:24:58 +0900

    feat: 채팅 내역 조회 시간 추가
    - 채팅 내역 조회 시간 필드 추가
    - ChatMessageResDTO 필드 추가

--- src/main/java/com/kakaoseventeen/dogwalking/chat/dto/ChatMessageResDTO.java
+++ src/main/java/com/kakaoseventeen/dogwalking/chat/dto/ChatMessageResDTO.java
@@ -3,10 +3,12 @@ package com.kakaoseventeen.dogwalking.chat.dto;
 import com.kakaoseventeen.dogwalking.chat.domain.MessageType;
 import lombok.Builder;
 
+
 public record ChatMessageResDTO(
         String content,
         MessageType messageType,
-        Long memberId
+        Long memberId,
+        Long createdAt
 ) {
     @Builder
     public ChatMessageResDTO{
