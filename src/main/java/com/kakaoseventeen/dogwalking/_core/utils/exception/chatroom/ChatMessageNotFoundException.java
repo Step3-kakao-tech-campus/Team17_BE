e45673a2decf671c856b913fceeacd76f8ed5b47
Cookie <pon05114@naver.com>
2023-11-11 20:29:25 +0900

    refactor: 미사용 import 삭제
    - 미사용 import 삭제
    - 코드 컨벤션 적용

--- src/main/java/com/kakaoseventeen/dogwalking/_core/utils/exception/chatroom/ChatMessageNotFoundException.java
+++ src/main/java/com/kakaoseventeen/dogwalking/_core/utils/exception/chatroom/ChatMessageNotFoundException.java
@@ -1,7 +1,9 @@
 package com.kakaoseventeen.dogwalking._core.utils.exception.chatroom;
 
 import com.kakaoseventeen.dogwalking._core.utils.ChatRoomMessageCode;
+import lombok.Getter;
 
+@Getter
 public class ChatMessageNotFoundException extends RuntimeException{
 
     private ChatRoomMessageCode chatRoomMessageCode;
